72a7eaba20d8380e8f3f2039adcdcafc0d2da716
Joaoviana <joaopadraoviana@gmail.com>
2018-09-21 15:48:07 +0200

    set private routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import './firebase/firebase';
+import {firebase} from './firebase/firebase';
 
-import AppRouter from './routers/AppRouter';
+
+import AppRouter, {history}from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
+import {login,logout} from './actions/auth';
 import getVisibileExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
@@ -22,10 +23,30 @@ const jsx = (
     </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+    if(!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
+
+
+firebase.auth().onAuthStateChanged((user) => {
+    if(user){
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+           renderApp();
+           if (history.location.pathname==='/') {
+               history.push('/dashboard');
+           }
+        });
+    }else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
 });
-
-
